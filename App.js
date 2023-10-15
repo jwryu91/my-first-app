32cdff20e79dc12456238148788503eea9145fd5
DESKTOP-UAS8AGU\jwryu <fbrlgns23@gmail.com>
2023-10-15 10:21:37 +0900

    no message

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>안녕하세요!!</Text>
+      <Text>안녕하세요!!수정된거에요!!</Text>
       <StatusBar style="auto" />
     </View>
   );
